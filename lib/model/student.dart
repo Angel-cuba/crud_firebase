f7c736e9e140699c1b89d0844f783bdc8dcd2f2b
Mac_Developer <araozangel842@gmail.com>
2023-01-28 15:16:15 +0200

    Add student model

---
+++ lib/model/student.dart
@@ -0,0 +1,37 @@
+// To parse this JSON data, do
+//
+//     final student = studentFromJson(jsonString);
+
+import 'dart:convert';
+
+Student studentFromJson(String str) => Student.fromJson(json.decode(str));
+
+String studentToJson(Student data) => json.encode(data.toJson());
+
+class Student {
+  Student({
+    required this.id,
+    required this.rollno,
+    required this.name,
+    required this.level,
+  });
+
+  final String id;
+  final int rollno;
+  final String name;
+  final int level;
+
+  factory Student.fromJson(Map<String, dynamic> json) => Student(
+        id: json["id"],
+        rollno: json["rollno"],
+        name: json["name"],
+        level: json["level"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "rollno": rollno,
+        "name": name,
+        "level": level,
+      };
+}
