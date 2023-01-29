7fe82ba941aec18bfe7b9fbb11ca9b097b9221a0
Mac_Developer <araozangel842@gmail.com>
2023-01-29 14:43:53 +0200

    Fetch data and delete a single student

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:crud_firebase/crud/add_student.dart';
 import 'package:crud_firebase/crud/update_student.dart';
 import 'package:crud_firebase/model/student.dart';
@@ -11,6 +12,9 @@ class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
+
+final CollectionReference studentsCollection =
+    FirebaseFirestore.instance.collection('students');
 
 class _HomePageState extends State<HomePage> {
   List<Student> students = [
@@ -27,6 +31,14 @@ class _HomePageState extends State<HomePage> {
   ];
   //List of images to render
   List<String> images = [
+    'https://picsum.photos/250?image=31',
+    'https://picsum.photos/250?image=2',
+    'https://picsum.photos/250?image=13',
+    'https://picsum.photos/250?image=4',
+    'https://picsum.photos/250?image=25',
+    'https://picsum.photos/250?image=6',
+    'https://picsum.photos/250?image=7',
+    'https://picsum.photos/250?image=8',
     'https://picsum.photos/250?image=31',
     'https://picsum.photos/250?image=2',
     'https://picsum.photos/250?image=13',
@@ -52,91 +64,38 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.redAccent.shade700,
         title: Text("TODO ${widget.title}"),
       ),
-      body: students.isEmpty
-          ? const Center(
-              child: Text('No students found'),
-            )
-          : Padding(
-              padding: EdgeInsets.only(bottom: fullHeight * 0.019),
-              child: Column(
-                children: [
-                  Container(
-                    height: fullHeight * 0.1,
-                    width: fullWidth,
-                    color: Colors.redAccent.shade700,
-                    child: const Center(
-                      child: Text(
-                        'Students',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Text(
-                    'List of students',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: students.length,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          elevation: 5,
-                          color: students[index].level < 3
-                              ? Colors.deepOrangeAccent.shade100
-                              : students[index].level < 7
-                                  ? Colors.green.shade400
-                                  : Colors.blue.shade400,
-                          child: ListTile(
-                              leading: CircleAvatar(
-                                backgroundImage: NetworkImage(images[index]),
-                              ),
-                              title: Text(students[index].name),
-                              subtitle: Text('Level: ${students[index].level}'),
-                              trailing: SizedBox(
-                                width: fullWidth * 0.3,
-                                child: Row(
-                                  children: [
-                                    IconButton(
-                                      onPressed: () {
-                                        Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (context) =>
-                                                    UpdateStudent(
-                                                      student: students[index],
-                                                    )));
-                                      },
-                                      icon: const Icon(Icons.edit),
-                                      color: Colors.green,
-                                    ),
-                                    IconButton(
-                                      onPressed: () {},
-                                      icon: const Icon(Icons.delete),
-                                      color: Colors.red.shade600,
-                                    ),
-                                  ],
-                                ),
-                              )),
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
+      body: FutureBuilder<QuerySnapshot>(
+        future: studentsCollection.get(),
+        builder: (context, snapshot) {
+          // ?Throw error if any
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('Something went wrong'),
+            );
+          }
+
+          if (snapshot.hasData) {
+            //! Firebase data
+            QuerySnapshot? data = snapshot.data!;
+            List<QueryDocumentSnapshot> studentsData = data.docs;
+            //!Convert the list of documents to list of students
+            List<Student> allStudents = studentsData
+                .map((e) => Student(
+                    id: e['id'],
+                    name: e['name'],
+                    level: e['level'],
+                    rollno: e['rollno']))
+                .toList();
+            return getStudents(
+                context, allStudents, fullHeight, fullWidth, images);
+          } else {
+            // ?Loading
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
       floatingActionButton: FloatingActionButton.extended(
           backgroundColor: Colors.redAccent.shade700,
           elevation: 6,
@@ -152,4 +111,95 @@ class _HomePageState extends State<HomePage> {
           icon: const Icon(Icons.pages)),
     );
   }
+}
+
+Widget getStudents(context, students, fullHeight, fullWidth, images) {
+  return students.isEmpty
+      ? const Center(
+          child: Text('No students found'),
+        )
+      : Padding(
+          padding: EdgeInsets.only(bottom: fullHeight * 0.019),
+          child: Column(
+            children: [
+              Container(
+                height: fullHeight * 0.1,
+                width: fullWidth,
+                color: Colors.redAccent.shade700,
+                child: const Center(
+                  child: Text(
+                    'Students',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'List of students',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: students.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      elevation: 5,
+                      color: students[index].level < 3
+                          ? Colors.deepOrangeAccent.shade100
+                          : students[index].level < 7
+                              ? Colors.green.shade400
+                              : Colors.blue.shade400,
+                      child: ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(images[index]),
+                          ),
+                          title: Text(students[index].name),
+                          subtitle: Text('Level: ${students[index].level}'),
+                          trailing: SizedBox(
+                            width: fullWidth * 0.3,
+                            child: Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => UpdateStudent(
+                                                  student: students[index],
+                                                )));
+                                  },
+                                  icon: const Icon(Icons.edit),
+                                  color: Colors.green,
+                                ),
+                                IconButton(
+                                  onPressed: () {
+                                    studentsCollection
+                                        .doc(students[index].id)
+                                        .delete();
+                                  },
+                                  icon: const Icon(Icons.delete),
+                                  color: Colors.red.shade600,
+                                ),
+                              ],
+                            ),
+                          )),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        );
 }
