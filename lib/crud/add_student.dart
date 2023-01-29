f2f49e760294cdc4cb7e5bab1038bc5ca53a702d
Mac_Developer <araozangel842@gmail.com>
2023-01-29 03:44:09 +0200

    Adding a student to Firebase db
    Alert for empty fields and success message after submit data.

--- lib/crud/add_student.dart
+++ lib/crud/add_student.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crud_firebase/model/student.dart';
 import 'package:flutter/material.dart';
 
 class AddStudent extends StatefulWidget {
@@ -104,33 +106,75 @@ class _AddStudentState extends State<AddStudent> {
         elevation: 5,
         backgroundColor: Colors.blueAccent.shade400,
         onPressed: () {
-          Navigator.pop(context);
+          if (_nameController.text.isEmpty ||
+              _rollNoController.text.isEmpty ||
+              _levelController.text.isEmpty) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                padding: const EdgeInsets.only(left: 20, right: 15),
+                duration: const Duration(seconds: 2),
+                animation: CurvedAnimation(
+                    parent: const AlwaysStoppedAnimation(1),
+                    curve: Curves.elasticInOut),
+                behavior: SnackBarBehavior.floating,
+                margin:
+                    const EdgeInsets.only(bottom: 3, left: 13.0, right: 13.0),
+                shape: ShapeBorder.lerp(
+                    const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    5),
+                content: const Text('Please fill all fields',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 26)),
+                backgroundColor: Colors.red,
+                showCloseIcon: true,
+                closeIconColor: Colors.white70,
+              ),
+            );
+            return;
+          } else {
+            Student student = Student(
+                name: _nameController.text,
+                rollno: int.parse(_rollNoController.text),
+                level: int.parse(_levelController.text));
+
+            // print(student.toJson());
+            debugPrint(student.toJson().toString());
+            addStudentToFirebase(student, context);
+          }
         },
         child: const Icon(Icons.save_alt_rounded),
       ),
     );
   }
+}
 
-  Widget _buildTextField(
-      {required String label,
-      required String placeholder,
-      TextInputType textInputType = TextInputType.name,
-      required TextEditingController controller,
-      FocusNode? focusNode}) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextField(
-        focusNode: focusNode,
-        keyboardType: textInputType,
-        controller: controller,
-        decoration: InputDecoration(
-          border: const OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10.0)),
-          ),
-          labelText: label,
-          hintText: placeholder,
-        ),
+void addStudentToFirebase(Student student, BuildContext context) {
+  //! Add student to firebase
+  final studentRef = FirebaseFirestore.instance.collection('students');
+  //Initialize student id
+  student.id = studentRef.doc().id;
+  //Student object to jso
+  final studentData = student.toJson();
+  //?Add student to firebase
+  studentRef.doc(student.id).set(studentData).then((value) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Student added successfully!'),
+        backgroundColor: Colors.green,
       ),
     );
-  }
+    Navigator.pop(context);
+  }).catchError((error) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Failed to add student'),
+        backgroundColor: Colors.red,
+      ),
+    );
+  });
 }
